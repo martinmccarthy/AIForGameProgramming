224a7d2b154c52c647bdab3a7d4a27fc5f3f93b9
Martin McCarthy <ma310770@ucf.edu>
2026-01-31 15:28:28 -0500

    creates methods for each of the useful input buttons on the controller

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private InputActionReference aButton;
+    [SerializeField] private InputActionReference bButton;
+    [SerializeField] private InputActionReference xButton;
+    [SerializeField] private InputActionReference yButton;
+
+    [SerializeField] private InputActionReference leftTrigger;
+    [SerializeField] private InputActionReference leftGrip;
+    [SerializeField] private InputActionReference rightTrigger;
+    [SerializeField] private InputActionReference rightGrip;
+
+
+    /* these are most likely going to be only read values with no bindings to them but i'll leave them here for now */
+    [SerializeField] private InputActionReference hmdPosition;
+    [SerializeField] private InputActionReference leftControllerPosition;
+    [SerializeField] private InputActionReference rightControllerPosition;
+    [SerializeField] private InputActionReference leftControllerRotation;
+    [SerializeField] private InputActionReference rightControllerRotation;
+
+    private void OnAwake()
+    {
+        aButton.action.performed += PressAButton;
+        bButton.action.performed += PressBButton;
+        xButton.action.performed += PressXButton;
+        yButton.action.performed += PressYButton;
+        leftTrigger.action.performed += PressLeftTrigger;
+        leftGrip.action.performed += PressLeftGrip;
+        rightTrigger.action.performed += PressRightTrigger;
+        rightGrip.action.performed += PressRightGrip;
+    }
+
+    void PressAButton(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    void PressBButton(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    void PressXButton(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    void PressYButton(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    void PressLeftTrigger(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    void PressRightTrigger(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    void PressLeftGrip(InputAction.CallbackContext ctx)
+    {
+
+    }
+    void PressRightGrip(InputAction.CallbackContext ctx)
+    {
+
+    }
+}
