2d0af704986f94e6ed5c0c480b02d2e4fbbd0222
Martin McCarthy <ma310770@ucf.edu>
2026-02-18 22:13:10 -0500

    adds enum for attacktypes and moves input tracking into the inputmanager

--- Assets/Scripts/SwordManager.cs
+++ Assets/Scripts/SwordManager.cs
@@ -2,64 +2,52 @@ using UnityEngine;
 
 public class SwordManager : MonoBehaviour
 {
-    // Controller Input Values
-    [SerializeField] private Transform leftControllerTransform;
-    [SerializeField] private Transform rightControllerTransform;
-    // settings for the thresholds of speeds and what not
-    [SerializeField, Range(0f, 10f)] private float MIN_SWIPE_SPEED = 1.5f;
-    [SerializeField, Range(0f, 1f)] private float MIN_ANGLE_THRESHOLD = 0.6f;
-
-    // this will allow us to determine which hand the sword should be bound to in the future based on a settings option
-    private bool playerLefty = false; // playerLefty = userSettings.hand in start or something like that;
-    private Transform activeController;
-
-    private Vector3 lastPosition;
-    private float lastTime;
+    [SerializeField] InputManager inputManager;
 
     private void Start()
     {
-        activeController = playerLefty ? leftControllerTransform : rightControllerTransform;
-        lastPosition = activeController.position;
-        lastTime = Time.time;
+
     }
 
     private void Update()
     {
-        MotionCheck(activeController.position);
+        if (inputManager != null)
+        {
+            AttackTypes attackState = inputManager.MotionCheck();
+            switch(attackState)
+            {
+                case AttackTypes.SwipeDown:
+                    DownwardAttack();
+                    break;
+                case AttackTypes.Stab:
+                    StabAttack();
+                    break;
+                case AttackTypes.Generic:
+                    GenericAttack();
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
-    private void MotionCheck(Vector3 controllerPosition)
+    private void DownwardAttack()
     {
-        float currentTime = Time.time;
-        float deltaTime = currentTime - lastTime;
 
-        Vector3 velocity = (controllerPosition - lastPosition) / deltaTime;
-        Vector3 direction = velocity.normalized;
+    }
 
-        if(velocity.magnitude > MIN_SWIPE_SPEED)
-        {
-            return;
-        }
+    private void StabAttack()
+    {
 
-        bool movingForward = Vector3.Dot(direction, Vector3.forward) > MIN_ANGLE_THRESHOLD; // this will probably also need to validate the rotation of the controller
-        if (movingForward)
-        {
-            Debug.Log("Swipe Forward Detected");
-        }
+    }
 
-        bool movingDown = Vector3.Dot(direction, Vector3.down) > MIN_ANGLE_THRESHOLD;
-        
-        if (movingDown)
-        {
-            Debug.Log("Swipe Down Detected");
-        }
+    private void GenericAttack()
+    {
 
-        if(!movingDown && !movingForward)
-        {
-            Debug.Log("Generic Swipe Detected");
-        }
+    }
 
-        lastPosition = controllerPosition;
-        lastTime = currentTime;
+    private void DoDamage(int amount)
+    {
+
     }
 }
