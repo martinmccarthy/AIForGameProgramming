44c4b8b02bf92d30f47523fc303a4c1806ddc27d
Taylor Harry <160172443+Idle-Truth@users.noreply.github.com>
2026-02-19 21:57:43 -0500

    Healthbar_v1

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,6 +1,51 @@
+//All values are for testing purposes only they will be updated as things progress 
+
+
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
+    
+    [SerializeField] private Slider healthBar;
+    public int health = 100;
+
+    void Start()
+    {
+        healthBar.value = 1f;
+        health = 100;
+    }
+
+    private void Update()
+    {
+        
+    }
+    
+    private void TakeDamage()
+    {
+        health -= 10;
+        healthBar.value = healthBar.value - 0.1f;
+    }
+
+    private void Heal()
+    {
+        health += 10;
+        healthBar.value = healthBar.value +  0.1f;
+        
+    }
+    
+    [ContextMenu("Test Heal 10")]
+    private void TestHeal()
+    {
+        Heal();
+    }
+
+    [ContextMenu("Test Damage 10")]
+    private void TestDamage()
+    {
+        TakeDamage();
+    }
+    
     
 }
